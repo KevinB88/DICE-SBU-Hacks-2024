05bfefedf6eaeb69038fba5fd78645335c47d48c
giancarlo forero <giancarloforero@gmail.com>
2024-02-10 02:42:20 -0500

    initial commit

---
+++ frontEnd/StartPage.js
@@ -0,0 +1,6 @@
+document.querySelector('.close-button').addEventListener('click', function() {
+  document.querySelector('.modal').style.display = 'none';
+});
+
+// Add event listeners for social buttons if needed
+
